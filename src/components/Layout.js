7ee5cfcf4ebde8886f2ae9ce70acf49403e7fcf0
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-16 22:57:49 -0500

    Modified the app layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import { css } from '@emotion/core'
-import { ryhthm } from '../utils/typography'
+import { rhythm } from '../utils/typography'
+import Header from './Header'
 
 export default ({ children }) => {
-	return <div css={css`max-width: 750px;width: 100%;`}>
-		{children}
+	return <div
+	>
+		<div css={css`
+			margin: 0 auto;
+			max-width: 700px;
+			padding: ${rhythm(2)};
+			padding-top: ${rhythm(1.5)};
+			position: relative;
+		`}>
+			<Header>
+				<Link to="/">
+				<h3
+						css={css`
+							margin-bottom: ${rhythm(2)};
+							display: inline-block;
+							font-style: normal;
+						`}
+					>
+						Corgi World
+				</h3>
+				</Link>
+				<Link css={css`float: right`} to="/about">
+					But why?
+				</Link>
+			</Header>
+			{children}
+		</div>
 	</div>
 }
