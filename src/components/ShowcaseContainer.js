d0db459b7181c598e6db91ac3e25eb4b325f1e77
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-16 23:18:02 -0500

    added some media queries

--- src/components/ShowcaseContainer.js
+++ src/components/ShowcaseContainer.js
@@ -7,6 +7,11 @@ export default ({ children }) => (
 		grid-template-columns: repeat(2, 1fr);
 		grid-gap: 0em 1em;
 		margin: 1em 0;
+		@media (max-width: 576px) {
+			margin: 1em 1em;
+			grid-template-columns: 1fr;
+			grid-gap: 1em;
+		}
 	`}>
 	{children}
 	</div>
