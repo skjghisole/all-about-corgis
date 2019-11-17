e04f5bc26b644b7eb1b436fb31ee48ab4c4ad883
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-16 21:54:42 -0500

    implemented the layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import { css } from '@emotion/core'
+import { ryhthm } from '../utils/typography'
 
-export default ({ children }) => (
-	
-)
+export default ({ children }) => {
+	return <div css={css`
+		max-width: 750px;
+		width: 100%;
+	`}>
+		{children}
+	</div>
+}
