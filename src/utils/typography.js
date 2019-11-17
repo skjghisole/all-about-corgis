b1e2a2b5897a68fab2b9a75deed87a1d8b586183
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-16 21:55:18 -0500

    implemented typography

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from 'typography'
+import customTheme from 'typography-theme-kirkham'
+
+
+const typography = new Typography(customTheme)
+export const { rhythm } = typography 
+
+export default typography
