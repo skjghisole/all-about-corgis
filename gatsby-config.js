c61e2fa3e5cb07091c3eb419672c46148e175c82
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-18 00:30:00 -0500

    Added some C

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,27 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `Why I love Corgis`,
+    description: `I dont know why, but corgis are the best`,
+    author: `skjghisole`,
+  },
   /* Your site config here */
   plugins: [
   	`gatsby-plugin-emotion`,
   	`gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Corgi World`,
+        short_name: `Corgi`,
+        start_url: `/about`,
+        background_color: `white`,
+        theme_color: `white`,
+        display: `standalone`,
+        icon: `src/assets/images/corgi1.jpg`
+      }
+    },
   	{
   		resolve: `gatsby-source-filesystem`,
   		options: {
@@ -21,6 +38,8 @@ module.exports = {
   		options: {
   			pathToConfigModule: `src/utils/typography`
   		}
-  	}
+  	},
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ]
 }
