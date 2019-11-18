a6eef6f22d7d0a786455cb15a37b7dc91bdc7893
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-18 00:32:54 -0500

    added a 404 route for pages that are not found

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default () => {
+	return <Layout>
+		Sorry, We cannot find the page you are looking for.
+	</Layout>
+}
